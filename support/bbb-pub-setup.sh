7b0ba72ca6da3b8b4cf790328178bc2bb8915276
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2021-09-22 10:28:54 +0530

    Update bbb-pub-setup.sh

--- support/bbb-pub-setup.sh
+++ support/bbb-pub-setup.sh
@@ -7,7 +7,7 @@ echo 'Setting up bbb for :'
 echo $1
 echo $2
 wget -qO- https://ubuntu.bigbluebutton.org/bbb-install.sh | bash -s -- -w -a -v bionic-240 -s $1 -e $2
-git clone https://github.com/afsaredrisy/bbb-install-aws-change.git
+git clone https://github.com/sasidhar-serv/bbb-install.git
 cd bbb-install-aws-change
 cd support/
 chmod +x support.sh
