dd7b1e70a8f58a8b41e0536eb7a59f990d61a4ef
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2021-11-11 11:40:33 +0530

    Update post2.sh

--- support/post2.sh
+++ support/post2.sh
@@ -2,16 +2,16 @@
 
 echo 'Downloading files to /var/www/bigbluebutton-default'
 cd /var/www/bigbluebutton-default
-wget -O default.pdf https://entrarlive.s3.ap-south-1.amazonaws.com/entrarlive/default.pdf
-wget -O index.html https://entrarlive.s3.ap-south-1.amazonaws.com/entrarlive/index.html
-wget -O favicon.ico https://entrarlive.s3.ap-south-1.amazonaws.com/entrarlive/favicon.ico
-wget -O favicon.png https://entrarlive.s3.ap-south-1.amazonaws.com/entrarlive/favicon.png
+wget -O default.pdf http://www.entrar.in/attachments/11/entrarlive/default.pdf
+wget -O index.html http://www.entrar.in/attachments/11/entrarlive/index.html
+wget -O favicon.ico http://www.entrar.in/attachments/11/entrarlive/favicon.ico
+wget -O favicon.png http://www.entrar.in/attachments/11/entrarlive/favicon.png
 
 echo 'Downloading file to /var/www/bigbluebutton-default/images'
 cd /var/www/bigbluebutton-default/images
-wget -O favicon.ico https://entrarlive.s3.ap-south-1.amazonaws.com/entrarlive/favicon.ico
-wget -O favicon.png https://entrarlive.s3.ap-south-1.amazonaws.com/entrarlive/favicon.png
-wget -O bbb-logo.png https://entrarlive.s3.ap-south-1.amazonaws.com/entrarlive/bbb-logo.png
+wget -O favicon.ico http://www.entrar.in/attachments/11/entrarlive/favicon.ico
+wget -O favicon.png http://www.entrar.in/attachments/11/entrarlive/favicon.png
+wget -O bbb-logo.png http://www.entrar.in/attachments/11/entrarlive/bbb-logo.png
 echo 'File download done'
 
 
