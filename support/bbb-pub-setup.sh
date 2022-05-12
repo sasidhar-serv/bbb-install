fe799766af03d9c48350ed79ad667056a7eadf36
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2022-05-12 19:23:47 +0530

    Update bbb-pub-setup.sh

--- support/bbb-pub-setup.sh
+++ support/bbb-pub-setup.sh
@@ -6,7 +6,7 @@ email=$2
 echo 'Setting up bbb for :'
 echo $1
 echo $2
-wget -qO- https://ubuntu.bigbluebutton.org/bbb-install.sh | bash -s -- -v bionic-240 -s $1 -e $2
+wget -qO- https://ubuntu.bigbluebutton.org/bbb-install.sh | bash -s -- -w -a -v bionic-23 -s $1 -e $2
 git clone https://github.com/sasidhar-serv/bbb-install.git
 cd bbb-install
 cd support/
