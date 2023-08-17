febcc9deb20a4bf444869d3a9d4472e23a64d947
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2023-08-17 10:47:09 +0530

    Update post2.sh

--- support/post2.sh
+++ support/post2.sh
@@ -2,15 +2,15 @@
 
 echo 'Downloading files to /var/www/bigbluebutton-default'
 cd /var/www/bigbluebutton-default
-wget -O default.pdf http://www.entrar.in/attachments/11/entrarlive/default.pdf
-wget -O index.html http://www.entrar.in/attachments/11/entrarlive/index.html
-wget -O favicon.ico http://www.entrar.in/attachments/11/entrarlive/favicon.ico
-wget -O favicon.png http://www.entrar.in/attachments/11/entrarlive/favicon.png
+wget -O default.pdf https://www.entrar.in/attachments/11/entrarlive/default.pdf
+wget -O index.html https://www.entrar.in/attachments/11/entrarlive/index.html
+wget -O favicon.ico https://www.entrar.in/attachments/11/entrarlive/favicon.ico
+wget -O favicon.png https://www.entrar.in/attachments/11/entrarlive/favicon.png
 
 echo 'Downloading file to /var/www/bigbluebutton-default/images'
 cd /var/www/bigbluebutton-default/images
-wget -O favicon.ico http://www.entrar.in/attachments/11/entrarlive/favicon.ico
-wget -O favicon.png http://www.entrar.in/attachments/11/entrarlive/favicon.png
-wget -O bbb-logo.png http://www.entrar.in/attachments/11/entrarlive/bbb-logo.png
+wget -O favicon.ico https://www.entrar.in/attachments/11/entrarlive/favicon.ico
+wget -O favicon.png https://www.entrar.in/attachments/11/entrarlive/favicon.png
+wget -O bbb-logo.png https://www.entrar.in/attachments/11/entrarlive/bbb-logo.png
 echo 'File download done'
 
