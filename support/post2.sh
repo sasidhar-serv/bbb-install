594801eb1798686470e8ad1b36b4f31de161f659
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2023-09-28 11:26:25 +0530

    post2.sh
    Changes in default.pdf
    changes in favicon

--- support/post2.sh
+++ support/post2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo 'Downloading files to /var/www/bigbluebutton-default'
-cd /var/www/bigbluebutton-default
+cd /var/www/bigbluebutton-default/assets/
 wget -O default.pdf https://www.entrar.in/attachments/11/entrarlive/default.pdf
 wget -O index.html https://www.entrar.in/attachments/11/entrarlive/index.html
 wget -O favicon.ico https://www.entrar.in/attachments/11/entrarlive/favicon.ico
@@ -14,3 +14,9 @@ wget -O favicon.png https://www.entrar.in/attachments/11/entrarlive/favicon.png
 wget -O bbb-logo.png https://www.entrar.in/attachments/11/entrarlive/bbb-logo.png
 echo 'File download done'
 
+echo 'Downloading file to /var/www/bigbluebutton-default/'
+cd /var/www/bigbluebutton-default/
+wget -O favicon.ico https://www.entrar.in/attachments/11/entrarlive/favicon.ico
+wget -O favicon.png https://www.entrar.in/attachments/11/entrarlive/favicon.png
+wget -O bbb-logo.png https://www.entrar.in/attachments/11/entrarlive/bbb-logo.png
+echo 'File download done'
