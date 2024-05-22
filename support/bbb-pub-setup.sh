fb7d43c4fdc828be03bb60a434cd436046b61d8b
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2024-05-22 11:47:01 +0530

    Update bbb-pub-setup.sh

--- support/bbb-pub-setup.sh
+++ support/bbb-pub-setup.sh
@@ -7,7 +7,7 @@ email=$2
 echo 'Setting up bbb for :'
 echo $1
 echo $2
-wget -qO- https://ubuntu.bigbluebutton.org/bbb-install-2.6.sh | bash -s -- -v focal-260 -s $1 -e $2 -g -w
+wget -qO- https://raw.githubusercontent.com/bigbluebutton/bbb-install/v2.7.x-release/bbb-install.sh | bash -s -- -w -v focal-270 -s $1 -e $2 -g -w
 
 git clone https://github.com/sasidhar-serv/bbb-install.git
 cd bbb-install
