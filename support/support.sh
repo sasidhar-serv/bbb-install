389f5e6896afae3a20f3b9048b947c39024e36f3
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2025-04-28 17:15:50 +0530

    Update support.sh

--- support/support.sh
+++ support/support.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-chmod +x post.sh
-chmod +x post1.sh
-chmod +x post2.sh
-chmod +x changejsession.sh
-bash post.sh
-bash post1.sh
-bash post2.sh
-bash changejsession.sh
-sudo bbb-conf --restart
+#chmod +x post.sh
+#chmod +x post1.sh
+#chmod +x post2.sh
+#chmod +x changejsession.sh
+#bash post.sh
+#bash post1.sh
+#bash post2.sh
+#bash changejsession.sh
+#sudo bbb-conf --restart
 
