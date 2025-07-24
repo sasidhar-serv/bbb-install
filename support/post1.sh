e3b5b70f6d5f6fd13bcbbf56c72bbe5dccefba0d
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2025-07-24 15:56:54 +0530

    Update post1.sh

--- support/post1.sh
+++ support/post1.sh
@@ -11,6 +11,6 @@ sed -i 's/displayBbbServerVersion: true/displayBbbServerVersion: false/g' /usr/s
 #Not working this currently
 #sed -i 's/cameraProfiles:\n    - id: low\n      name: Low quality\n      default: false\n      bitrate: 100\n    - id: medium\n      name: Medium quality\n      default: true\n      bitrate: 200\n    - id: high\n      name: High quality\n      default: false\n      bitrate: 500\n    - id: hd\n      name: High definition\n      default: false\n      bitrate: 800/cameraProfiles:\n    - id: low\n      name: Low quality\n      default: false\n      bitrate: 50\n    - id: medium\n      name: Medium quality\n      default: true\n      bitrate: 100\n    - id: high\n      name: High quality\n      default: false\n      bitrate: 200\n    - id: hd\n      name: High definition\n      default: false\n      bitrate: 400\n/g' /home/introtuce/kpcc/bbb-install-master/postInstallation/settingdemo.yml
 
-echo 'Please change CameraProfile manually could not change by script'
+echo 'Please change CameraProfile manually could not change by script' 
 
 echo '================================================================================'
