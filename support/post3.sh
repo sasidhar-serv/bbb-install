fc922db4d7873ab8a06ae80d354d2d64570b6571
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2025-07-25 23:10:01 +0530

    Update post3.sh
    DOne changes for sed

--- support/post3.sh
+++ support/post3.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 echo 'Changing /var/bigbluebutton/learning-dashboard/static/js/main.808d2575.js'
-echo '================================================================================
-sed -i 's/"app\.learningDashboard\.bigbluebuttonTitle",defaultMessage:"BigBlueButton"/"Entrar",defaultMessage:"Entrar"/g' /var/bigbluebutton/learning-dashboard/static/js/main.808d2575.js
+echo '================================================================================'
+
+sed -i "s/\"app\.learningDashboard\.bigbluebuttonTitle\",defaultMessage:\"BigBlueButton\"/\"Entrar\",defaultMessage:\"Entrar\"/g" /var/bigbluebutton/learning-dashboard/static/js/main.808d2575.js
+echo "Changes done"
