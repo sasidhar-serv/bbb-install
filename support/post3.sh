0ef4330f80d0ba8da70c2654c5f0a96805dedb08
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2025-07-25 17:47:57 +0530

    Update post3.sh

--- support/post3.sh
+++ support/post3.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
 
+echo 'Changing /var/bigbluebutton/learning-dashboard/static/js/main.808d2575.js'
+echo '================================================================================'
